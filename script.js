0632901efb0c18a0c0e2f0cd0cee42813ccd9c30
Edgar <edwarr170484@gmail.com>
2021-10-22 04:11:35 +0300

    Fourth task solution (objects)

--- script.js
+++ script.js
@@ -7,42 +7,53 @@ let personalMovieDB = {
     movies: {},
     actors: {},
     genres: [],
-    privat: false
+    privat: false,
+    toggleVisibleMyDB(){
+        this.privat = !this.privat;
+    },
+    writeYourGenres(){
+        for(let i = 0;i < 3;i++){
+            this.genres[i] = prompt(`Ваш любимый жанр под номером ${i + 1}`);
+            
+            while(!this.genres[i] || this.genres[i] == ''){
+                this.genres[i] = prompt(`Ваш любимый жанр под номером ${i + 1}`);
+            }
+        }
+
+        this.genres.forEach((genre, index, genres) => {
+            console.log(`Любимый жанр ${index + 1} - это ${genre}`);
+        });
+    },
+    showMyDB(){
+        if(!this.privat){
+            console.log(this);
+        }
+    },
+    setFilmRanks(){
+        for(let i = 0;i < 2; i++){
+            let lastSeenFilm = prompt('Один из последних просмотренных фильмов?');
+            let lastSeenFilmRank = prompt('На сколько оцените его?');
+        
+            while(lastSeenFilm.length > 50 || lastSeenFilm == '' || lastSeenFilmRank == '' || lastSeenFilm === null || !lastSeenFilmRank === null){
+                lastSeenFilm = prompt('Один из последних просмотренных фильмов?');
+                lastSeenFilmRank = prompt('На сколько оцените его?');
+            }
+            
+            this.movies[lastSeenFilm] = lastSeenFilmRank;
+        }
+    },
+    whoAreYou(){
+        if(this.count < 10){
+            console.log('Просмотрено довольно мало фильмов');
+        }
+        if(this.count >=10 && this.count <= 30){
+            console.log('Вы классический зритель');
+        }
+        if(this.count > 30){
+            console.log('Вы киноман');
+        }
+    }
 };
 
-for(let i = 0;i < 2; i++){
-    let lastSeenFilm = prompt('Один из последних просмотренных фильмов?');
-    let lastSeenFilmRank = prompt('На сколько оцените его?');
 
-    while(lastSeenFilm.length > 50 || lastSeenFilm == '' || lastSeenFilmRank == '' || lastSeenFilm === null || !lastSeenFilmRank === null){
-        lastSeenFilm = prompt('Один из последних просмотренных фильмов?');
-        lastSeenFilmRank = prompt('На сколько оцените его?');
-    }
-    
-    personalMovieDB.movies[lastSeenFilm] = lastSeenFilmRank;
-}
-
-if(personalMovieDB.count < 10){
-    console.log('Просмотрено довольно мало фильмов');
-}
-if(personalMovieDB.count >=10 && personalMovieDB.count <= 30){
-    console.log('Вы классический зритель');
-}
-if(personalMovieDB.count > 30){
-    console.log('Вы киноман');
-}
-
-function showMyDB(dbObject){
-    if(!dbObject.privat){
-        console.log(dbObject);
-    }
-}
-
-function writeYourGenres(){
-    for(let i = 0;i < 3;i++){
-        personalMovieDB.genres[i] = prompt(`Ваш любимый жанр под номером ${i + 1}`);
-    }
-}
-
-writeYourGenres();
-showMyDB(personalMovieDB);
+personalMovieDB.writeYourGenres();

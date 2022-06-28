569c7ea3b4c549048eb62116f0e9379dc82b5f56
Edgar <edwarr170484@gmail.com>
2022-06-28 17:53:58 +0300

    Решение задачи

--- script.js
+++ script.js
@@ -1,59 +1,47 @@
 'use strict';
 
-let numberOfFilms = prompt('Сколько фильмов вы уже посмотрели?');
-
-let personalMovieDB = {
-    count: numberOfFilms,
-    movies: {},
-    actors: {},
-    genres: [],
-    privat: false,
-    toggleVisibleMyDB(){
-        this.privat = !this.privat;
-    },
-    writeYourGenres(){
-        for(let i = 0;i < 3;i++){
-            this.genres[i] = prompt(`Ваш любимый жанр под номером ${i + 1}`);
-            
-            while(!this.genres[i] || this.genres[i] == ''){
-                this.genres[i] = prompt(`Ваш любимый жанр под номером ${i + 1}`);
-            }
+function operate(number, operation = null){
+    if(operation){
+        switch(operation.kind){
+            case 'plus':
+                return number + operation.value;
+            break;
+            case 'minus':
+                return number - operation.value;
+            break;
+            case 'mul':
+                return number * operation.value;
+            break;
+            case 'div':
+                return number / operation.value;
+            break;
         }
-
-        this.genres.forEach((genre, index, genres) => {
-            console.log(`Любимый жанр ${index + 1} - это ${genre}`);
-        });
-    },
-    showMyDB(){
-        if(!this.privat){
-            console.log(this);
-        }
-    },
-    setFilmRanks(){
-        for(let i = 0;i < 2; i++){
-            let lastSeenFilm = prompt('Один из последних просмотренных фильмов?');
-            let lastSeenFilmRank = prompt('На сколько оцените его?');
-        
-            while(lastSeenFilm.length > 50 || lastSeenFilm == '' || lastSeenFilmRank == '' || lastSeenFilm === null || !lastSeenFilmRank === null){
-                lastSeenFilm = prompt('Один из последних просмотренных фильмов?');
-                lastSeenFilmRank = prompt('На сколько оцените его?');
-            }
-            
-            this.movies[lastSeenFilm] = lastSeenFilmRank;
-        }
-    },
-    whoAreYou(){
-        if(this.count < 10){
-            console.log('Просмотрено довольно мало фильмов');
-        }
-        if(this.count >=10 && this.count <= 30){
-            console.log('Вы классический зритель');
-        }
-        if(this.count > 30){
-            console.log('Вы киноман');
-        }
+    }else{
+        return number;
     }
-};
+}
 
+function one(operation = null){
+    return operate(1, operation);
+}
 
-personalMovieDB.writeYourGenres();
+function four(operation = null){
+    return operate(4, operation);
+}
+
+function plus(value){
+    return {
+        kind: 'plus',
+        value: value
+    }
+}
+
+function minus(value){
+    return {
+        kind: 'minus',
+        value: value
+    }
+}
+
+console.log(four(plus(one())));
+console.log(four(minus(one())));
